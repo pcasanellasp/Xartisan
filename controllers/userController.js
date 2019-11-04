77c0286dae63673b0718bd8923ff710d34268db5
Pau Casanellas <pcasanellasp@gmail.com>
2019-11-04 16:14:23 +0100

    User crud completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,13 +13,52 @@ async function get (req, res, next) {
   }
 }
 
+async function show (req, res, next) {
+  try {
+    const user = await User.findOne({ _id: req.params.id }).lean()
+    if (user) {
+      return res.status(200).json(user)
+    }
+    return res.status(400).json({ message: 'No User Found' })
+  } catch (error) {
+    next(error)
+  }
+}
+
 async function create (req, res, next) {
   // Create a new user
   try {
     const user = new User(req.body)
     await user.save()
     const token = await user.generateAuthToken()
-    res.status(201).send({ user, token })
+    res.status(201).json({ user, token })
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function update (req, res, next) {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      {
+        new: true,
+        runValidators: true,
+      },
+    )
+    return res.status(200).json(user)
+  } catch (error) {
+    error.statusCode = 403
+    next(error)
+  }
+}
+
+async function remove (req, res, next) {
+  // Remove user
+  try {
+    const user = await User.findByIdAndRemove(req.params.id)
+    res.status(202).json(user)
   } catch (error) {
     next(error)
   }
@@ -27,5 +66,8 @@ async function create (req, res, next) {
 
 module.exports = {
   get,
+  show,
   create,
+  update,
+  remove,
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,6 +9,9 @@ const { auth, allow } = require('../middlewares/auth')
 
 // --------------------
 router.get('/', auth, allow('admin'), userController.get)
+router.get('/:id', auth, allow('admin'), userController.show)
 router.post('/', auth, allow('admin'), userController.create)
+router.patch('/:id', auth, allow('admin'), userController.update)
+router.delete('/:id', auth, allow('admin'), userController.remove)
 
 module.exports = router
